6f205382a9b53f59d31bfc7c738ad1bf856ed20a
cassandra-mn <arthenciac@gmail.com>
2022-03-29 00:45:10 -0300

    layout da tela de entrar

--- src/Componentes/TelaEntrar.js
+++ src/Componentes/TelaEntrar.js
@@ -1,5 +1,70 @@
+import styled from 'styled-components';
+import logo from './../assets/images/logo.svg';
+
 export default function TelaEntrar() {
     return (
-        <>Tela entrar</>
+        <Container>
+            <Logo src={logo} alt='logo'></Logo>
+
+            <Imput type='email' required placeholder='email'></Imput>
+            <Imput type='password' required placeholder='senha'></Imput>
+            
+            <Button>Entrar</Button>
+            
+            <Cadastrar>Não tem uma conta? Cadastre-se!</Cadastrar>
+        </Container>
     );
 }
+
+const Container = styled.div `
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+const Logo = styled.img `
+    width: 180px;
+    height: 180px;
+    margin-top: 68px;
+    margin-bottom: 32px;
+`;
+
+const Imput = styled.input `
+    width: 303px;
+    height: 45px;
+    padding: 10px;
+    margin-bottom: 6px;
+    border-radius: 5px;
+    border: 1px solid #D5D5D5;
+    background: #FFFFFF;
+
+    ::placeholder {
+        color: #DBDBDB;
+    }
+`;
+
+const Button = styled.button `
+    width: 303px;
+    height: 45px;
+    font-size: 20px;
+    border-radius: 5px;
+    text-align: center;
+    border: none;
+    color: #FFFFFF;
+    background: #52B6FF;
+
+    :hover {
+        cursor: pointer;
+    }
+`
+
+const Cadastrar = styled.div `
+    width: 232px;
+    height: 17px;
+    font-size: 13px;
+    margin-top: 25px;
+    text-align: center;
+    font-family: 'Lexend Deca';
+    text-decoration-line: underline;
+    color: #52B6FF;
+`
--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import ReactDOM from 'react-dom';
 import App from './Componentes/App';
 
 import './css/reset.css';
+import './css/style.css';
 
 ReactDOM.render(<App />, document.querySelector('.root'));
