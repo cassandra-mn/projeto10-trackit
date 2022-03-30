9b1c8f65c61a802a1b393e98e5224ea29f23b564
cassandra-mn <arthenciac@gmail.com>
2022-03-30 11:32:11 -0300

    usuários autenticados

--- src/Componentes/TelaEntrar.js
+++ src/Componentes/TelaEntrar.js
@@ -1,18 +1,31 @@
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import axios from 'axios';
 import styled from 'styled-components';
 import logo from './../assets/images/logo.svg';
 
 export default function TelaEntrar() {
     const navigate = useNavigate();
+    const [dados, setDados] = useState({
+        email: '',
+        password: ''
+    });
+
+    function entrar() {
+        const URL = 'https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/auth/login'; 
+        const request = axios.post(URL, dados);
+        request.then(response => navigate('/habitos'))
+        .catch(error => alert(error.response.data.message));
+    }
 
     return (
         <Container>
             <Logo src={logo} alt='logo'></Logo>
 
-            <Imput type='email' required placeholder='email'></Imput>
-            <Imput type='password' required placeholder='senha'></Imput>
+            <Imput type='email' required placeholder='email' value={dados.email} onChange={e => setDados({...dados, email: e.target.value})}></Imput>
+            <Imput type='password' required placeholder='senha' value={dados.password} onChange={e => setDados({...dados, password: e.target.value})}></Imput>
             
-            <Button onClick={() => navigate('/habitos')}>Entrar</Button>
+            <Button onClick={() => entrar()}>Entrar</Button>
             
             <Cadastrar onClick={() => navigate('/cadastro')}>Não tem uma conta? Cadastre-se!</Cadastrar>
         </Container>
