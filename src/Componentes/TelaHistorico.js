a695c871ff452a292030e30321a2c1dcba2235cf
cassandra-mn <arthenciac@gmail.com>
2022-04-04 16:56:05 -0300

    clicando no dia incompleto

--- src/Componentes/TelaHistorico.js
+++ src/Componentes/TelaHistorico.js
@@ -8,6 +8,9 @@ import axios from 'axios';
 export default function TelaHistorico() {
     const { setVisivel } = useContext(UserContext);
     const [diasComHabitos, setDiasComHabitos] = useState();
+    const [escondido, setEscondido] = useState(true);
+    const [conteudo, setConteudo] = useState('');
+    const [habitos, setHabitos] = useState([]);
     const pegarDados = localStorage.getItem("dados");
     const novosDados = JSON.parse(pegarDados);
     setVisivel(true);
@@ -26,18 +29,31 @@ export default function TelaHistorico() {
 
     function classe({ date }) {
         if (diasComHabitos !== undefined) {
-            const dataC = diasComHabitos.find(dia => dia.day == dayjs(date).format('DD/MM/YYYY')); 
+            const dataC = diasComHabitos.find(dia => dia.day == dayjs(date).format('DD/MM/YYYY'));
             if (dataC) {
-                const teste = dataC.habits.find(data => data.done);
-                return teste ? 'completo' : 'incompleto';
+                const teste = dataC.habits.filter(data => data.done);
+                return teste.length === dataC.habits.length ? 'completo' : 'incompleto';
             }
+        }
+    }
+
+    function imprimir(dia) {
+        const teste = diasComHabitos.filter(d => d.day === dayjs(dia).format('DD/MM/YYYY'));
+        setConteudo(`Hábitos do dia ${dayjs(dia).format('DD/MM/YYYY')}:`);
+        setEscondido(false);
+        if (teste.length > 0) {
+            teste[0].habits.map(habito => setHabitos([...habitos, habito]));
+        }
+        else {
+            setConteudo('Nenhum hábito neste dia');
         }
     }
 
     return (
         <Container>
             <H1>Histórico</H1>
-            <Calendario><Calendar calendarType='US' tileClassName={classe} /></Calendario>
+            <Calendario><Calendar calendarType='US' onClickDay={dia => imprimir(dia)} tileClassName={classe} /></Calendario>
+            <p className={escondido ? 'escondido' : ''}>{conteudo}</p>
         </Container>
     );
 }
@@ -45,6 +61,14 @@ export default function TelaHistorico() {
 const Container = styled.div`
     font-family: 'Lexend Deca';
     margin-bottom: 100px;
+
+    p {
+        margin-left: 18px;
+    }
+
+    .escondido {
+        display: none;
+    }
 `;
 
 const H1 = styled.h1`
@@ -60,12 +84,22 @@ const Calendario = styled.div`
     margin: 20px;
 
     .completo {
-        border-radius: 50%;
-        background-color: #32CD32;
+        min-width: 30px;
+        min-height: 30px;
+        border-radius: 50px;
+        margin: auto;
+        color: white;
+        text-align: center;
+        background-color: #8CC654;
     }
 
     .incompleto {
-        border-radius: 50%;
-        background-color: #DC143C;
+        min-width: 30px;
+        min-height: 30px;
+        border-radius: 50px;
+        margin: auto;
+        color: white;
+        text-align: center;
+        background-color: #ff0000b4;
     }
 `;
